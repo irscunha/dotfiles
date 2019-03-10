cc9d17c167bcba99b4da1fe2897243854e501e7c
Ingrid Rosselis <irscunha@dcc.ufmg.br>
2019-03-10 14:11:33 -0300

    Adding move workspace mode on rofi

---
+++ rofi/scripts/move_workspace.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rofi_theme="$HOME/.config/rofi/themes/theme.rasi"
+options=$' 1\n 2\n 3\n 4\n 5\n 6\n 7\n 8'
+
+chosen="$(echo "$options" | rofi -dmenu -p ' workspace' -theme $rofi_theme)"
+
+case $chosen in
+	' 1') i3-msg move to workspace 1;;
+	' 2') i3-msg move to workspace 2;;
+	' 3') i3-msg move to workspace 3;;
+	' 4') i3-msg move to workspace 4;;
+	' 5') i3-msg move to workspace 5;;
+	' 6') i3-msg move to workspace 6;;
+	' 7') i3-msg move to workspace 7;;
+	' 8') i3-msg move to workspace 8;;
+esac
