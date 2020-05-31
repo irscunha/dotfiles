91fe8ddc89bdc08aef2594fa999786dee48e6b8c
Ingrid Rosselis <ingridrosselis@gmail.com>
2020-05-31 02:50:54 -0300

    Finish organizing files and rewriting the installation script

--- backup/backup.sh
+++
@@ -1,7 +0,0 @@
-cp -r /home/$1/.config/bspwm/ . &&
-cp -r /home/$1/.config/polybar/ . &&
-cp -r /home/$1/.config/rofi/ .
-cp -r /home/$1/.config/sxhkd/ .
-cp /home/$1/.xprofile X/
-cp /home/$1/.Xresources X/
-cp /home/$1/.vimrc vim/
