7367bb4f4048326c8eca433aad4518917ae5d548
Ingrid Rosselis <ingridrosselis@gmail.com>
2020-07-04 04:39:15 -0300

    Finish creating an installation script

--- install.sh
+++ install.sh
@@ -1,9 +1,53 @@
 #! /bin/zsh
 
+WALLPAPER_PATH=$1
 DOTFILES_REPO="git@github.com:innng/dotfiles.git"
 GIT_DIR="$HOME/.dotfiles"
 
+########## INSTALLATION ##########
+
+# update system
+sudo pacman -Syyu
+
+# install pip
+cd /tmp
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+
+# install yay
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -sicf --noconfirm --needed
+
+# install oh-my-zsh
+cd /tmp
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# install starship prompt
+curl -fsSL https://starship.rs/install.sh | zsh
+
+# install apps
+yay -S polybar python-dbus rofi code spotify flameshot telegram-desktop --noconfirm --needed
+
+
+# install powerline fonts
+cd /tmp
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+
+# install pywal
+cd /tmp
+pip install pywal
+
+########## CONFIGURATION ##########
+
+# apply pywal theme
+wal -i $WALLPAPER_PATH
+
 # setup dotifles config with git bare
+cd $HOME
 git clone --bare $DOTFILES_REPO $GIT_DIR
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles config status.showUntrackedFiles no
@@ -13,3 +57,4 @@ dotfiles pull
 
 # set vim as core editor for git
 git config --global core.editor "vim"
+
