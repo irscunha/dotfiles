f41911578f26e09b495eb8896abe57e1b2f9d7ea
Ingrid Rosselis <ingridrosselis@gmail.com>
2020-07-24 16:59:41 -0300

    Change polybar initialization

---
+++ .config/polybar/polybar_start.sh
@@ -0,0 +1,6 @@
+#! /bin/zsh
+
+killall polybar
+polybar -c $HOME/.config/polybar/config left-bar &
+# polybar -c $HOME/.config/polybar/config middle-bar &
+polybar -c $HOME/.config/polybar/config right-bar &
