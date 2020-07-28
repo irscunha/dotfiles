068f48a6fd67cf19dd1f4f0f1c751a0da4b3212d
Ingrid Rosselis <ingridrosselis@gmail.com>
2020-07-28 00:04:44 -0300

    Finish configuring polybar

--- .config/polybar/polybar_start.sh
+++ .config/polybar/polybar_start.sh
@@ -4,3 +4,4 @@ killall polybar
 polybar -c $HOME/.config/polybar/config left-bar &
 polybar -c $HOME/.config/polybar/config middle-bar &
 polybar -c $HOME/.config/polybar/config right-bar &
+polybar -c $HOME/.config/polybar/config support-bar &
