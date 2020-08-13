fd747b238a9b9d24b900d95a0a8651511269d359
Ingrid Rosselis <ingridrosselis@gmail.com>
2020-08-13 00:15:31 -0300

    Remove old files

--- .config/polybar/polybar_start.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/zsh
-
-killall polybar
-polybar -c $HOME/.config/polybar/config left-bar &
-polybar -c $HOME/.config/polybar/config middle-bar &
-polybar -c $HOME/.config/polybar/config right-bar &
-polybar -c $HOME/.config/polybar/config support-bar &
